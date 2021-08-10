cca46f0d024006542f441fc141037a0a7571ee7d
Andrew Sorensen <Sorensenapaul@gmail.com>
2021-08-09 21:52:22 -0700

    Got initial SSR working

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HomeComponent from "./components/Home";
+
+ReactDOM.render(
+    <HomeComponent />,
+    document.getElementById('app-container')
+);
---
+++ src/components/Home.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default class HomeComponent extends React.Component {
+
+    render() {
+        return <h1>The component is here!</h1>;
+    }
+}
